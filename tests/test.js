0af13446eb630265f8873a66174f100f86051382
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-15 15:25:56 +0530

    Update tests

--- tests/test.js
+++ tests/test.js
@@ -4,8 +4,8 @@ const processExists = require('process-exists');
 const BrKill = require('../index');
 const { ProcessName } = require('../lib');
 
-const ERROR_MESSAGE_CHROME = 'Error: Killing process Chrome failed: Process doesn\'t exist';
-const ERROR_MESSAGE_FIREFOX = 'Error: Killing process Firefox failed: Process doesn\'t exist';
+const ERROR_MESSAGE_CHROME = 'killing process chrome failed: process doesn\'t exist';
+const ERROR_MESSAGE_FIREFOX = 'killing process firefox failed: process doesn\'t exist';
 
 /**
  * !!!!!WARNING!!!!!!
@@ -15,10 +15,10 @@ describe('test kill browser process', () => {
   it('should kill chrome process', async () => {
     try  {
       await BrKill.chrome();
-      const exists = await processExists(ProcessName.chrome());      
+      const exists = await processExists(ProcessName.chrome());
       expect(exists).to.equal(false);
     } catch (err) {
-      expect(err.message.includes(ERROR_MESSAGE_CHROME)).to.equal(true);
+      expect(err.message.toLowerCase().includes(ERROR_MESSAGE_CHROME)).to.equal(true);
     }
   });
   it('should kill firefox process', async () => {
@@ -27,7 +27,7 @@ describe('test kill browser process', () => {
       const exists = await processExists(ProcessName.firefox());      
       expect(exists).to.equal(false);
     } catch (err) {
-      expect(err.message.includes(ERROR_MESSAGE_FIREFOX)).to.equal(true);
+      expect(err.message.toLowerCase().includes(ERROR_MESSAGE_FIREFOX)).to.equal(true);
     }
   });
 });
